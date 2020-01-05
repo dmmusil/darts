1b984e961c05aa7726cb4252c297bb2337e8c774
Dylan Musil <dmusil@degreed.com>
2020-01-05 15:24:28 -0500

    Add a console app so dotnet watch run can work with azure functions.

---
+++ Api/Darts.Api.Watcher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Darts.Api.Watcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = Directory.GetCurrentDirectory().Replace(".Watcher", "");
+            var info = new ProcessStartInfo
+            {
+                WorkingDirectory = dir,
+                FileName = "cmd.exe",
+                Arguments =
+                    "/K func host start --cors http://localhost:5000"
+            };
+            Process.Start(info).WaitForExit();
+        }
+    }
+}
